199afb5c4775b6076cead00fb90bd5b3abf2cf85
SanjeevKadam <45713166+SanjeevKadam@users.noreply.github.com>
2023-08-19 18:04:04 +0530

    Update Type.js

--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -7,8 +7,7 @@ function Type() {
       options={{
         strings: [
           "Software Developer",
-          "Freelancer",
-          "MERN Stack Developer",
+          "MEAN Stack Developer",
           "Open Source Contributor",
         ],
         autoStart: true,
